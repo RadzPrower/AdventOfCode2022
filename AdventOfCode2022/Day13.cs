bdfa0b15756f3d899c32ac7b5022fc33970ded91
ajohns6 <anthony.johns.6@gmail.com>
2022-12-12 17:33:18 -0500

    Preparatory work for Day #13

--- AdventOfCode2022/Day13.cs
+++ AdventOfCode2022/Day13.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace AdventOfCode_2022
 {
-    internal class Day13
+    internal class Day13 : AoC2022
     {
         internal static void Start()
         {
-            throw new NotImplementedException();
+            // Prompt the user to ask if this is a test
+            var lines = TestPrompt(13);
+
+            // Start the stopwatch to track execution time
+            var watch = Stopwatch.StartNew();
+
+            // Variable initialization
+
+
+            // Parse input
+
+
+            // Print results and performance summary
+            Summary(watch);
         }
     }
 }
